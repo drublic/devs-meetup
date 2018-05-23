63082fd580c1b95e29c86e26d59ab05e07807365
Sebastian Sebald <sebastian.sebald@gmail.com>
2018-05-23 15:28:14 +0200

    ✨ Header + Teaser

---
+++ src/scripts/dom.ts
@@ -0,0 +1,40 @@
+import { MeetupEvent } from './types';
+
+export const createLoadingSpinner = () =>
+  `<div class="teaser-loading">
+    <div class="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>`;
+
+const createCalendarIcon = (size = 24) =>
+  `<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 24 24">
+    <path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"/>
+  </svg>`;
+
+const formatTeaserDate = (value: string) =>
+  new Date(value).toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: 'short',
+  });
+
+export const createTeaser = (meetup: MeetupEvent) =>
+  `<div class="teaser">
+    <div class="teaser-date">
+      ${createCalendarIcon(20)}
+      ${formatTeaserDate(meetup.local_date)}
+    </div>
+    <h2 class="teaser-name">${meetup.name}</h2>
+    <div class="teaser-description">${meetup.description}</div>
+    <div class="teaser-footer">
+      <a class="teaser-link" href="${meetup.link}">More info & RSVP</a>
+    </div>
+  </div>`;
---
+++ src/scripts/index.ts
@@ -0,0 +1,28 @@
+import { createLoadingSpinner, createTeaser } from './dom';
+import { RequestStatus, request } from './request';
+import { MeetupEvent } from './types';
+
+const eventElement = document.getElementById('teaser')!;
+
+request<[MeetupEvent]>(
+  'https://api.meetup.com/Devsmeetup-Freiburg/events?&photo-host=public&page=1&status=upcoming',
+  ev => {
+    let content = '';
+
+    switch (ev.status) {
+      case RequestStatus.Fetching:
+        content = createLoadingSpinner();
+        break;
+      case RequestStatus.Success:
+        content = createTeaser(ev.data[0]);
+        break;
+      case RequestStatus.Error:
+        content = ev.error.message;
+        break;
+      default:
+        break;
+    }
+
+    eventElement.innerHTML = content;
+  }
+);
